7ee426499e915df918b9da4583687eba735fa3d9
Michael Kohler <me@michaelkohler.info>
2020-04-19 11:26:05 +0200

    Correctly handle error

--- index.js
+++ index.js
@@ -10,17 +10,22 @@ const DATA_PATH = path.join(__dirname, 'ui', 'src', 'DATA.json');
 process();
 
 async function process() {
-  const mentorshipStats = await bugzilla.getMentorshipStats();
-  const portalStats = await bugzilla.getPortalStats();
-  const budgetStats = await bugzilla.getBudgetStats();
-  const swagStats = await bugzilla.getSwagStats();
+  try {
+    const mentorshipStats = await bugzilla.getMentorshipStats();
+    const portalStats = await bugzilla.getPortalStats();
+    const budgetStats = await bugzilla.getBudgetStats();
+    const swagStats = await bugzilla.getSwagStats();
 
-  const data = {
-    mentorship: mentorshipStats,
-    portal: portalStats,
-    budget: budgetStats,
-    swag: swagStats,
-  };
+    const data = {
+      mentorship: mentorshipStats,
+      portal: portalStats,
+      budget: budgetStats,
+      swag: swagStats,
+    };
 
-  fs.writeFileSync(DATA_PATH, JSON.stringify(data));
+    fs.writeFileSync(DATA_PATH, JSON.stringify(data));
+  } catch (error) {
+    debug(error);
+  }
+
 }
